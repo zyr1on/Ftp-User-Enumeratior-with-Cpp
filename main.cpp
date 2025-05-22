6a70c25178efbfe98fee919603eeeec16d0e6287
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-05-22 15:11:15 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "Manager.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    std::string ip, filename;
+    int port = 21;
+    int threads = 4;
+    bool exitWhenFound = false;
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--ip" && i + 1 < argc) ip = argv[++i];
+        else if (arg == "--file" && i + 1 < argc) filename = argv[++i];
+        else if (arg == "--port" && i + 1 < argc) port = std::atoi(argv[++i]);
+        else if (arg == "--thread" && i + 1 < argc) threads = std::atoi(argv[++i]);
+        else if (arg == "--exitWhenFound" || arg == "--exitwhenfound" || arg == "-exitWhenFound" || arg == "-exitwhenfound") exitWhenFound = true;
+    }
+
+    if (ip.empty() || filename.empty()) {
+        std::cerr << "Usage:\n ./ftp_user_enum --ip <ip> --file <file> [--port <port>] [--thread <N>] [--exitWhenFound]\n\ndefault port: 21 | default thread:4\n";
+        return 1;
+    }
+
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "[-] File cannot opened: " << filename << "\n";
+        return 1;
+    }
+
+    std::vector<std::string> users;
+    std::string line;
+    while (std::getline(file, line)) {
+        if (!line.empty())
+            users.push_back(line);
+    }
+
+    Manager manager(ip, port, threads, users, exitWhenFound);
+    manager.run();
+
+    const auto& foundUsers = manager.getFoundUsers();
+    if (!foundUsers.empty()) {
+        std::cout << "\nVALID USERS:\n";
+        for (const auto& u : foundUsers) {
+            std::cout << " - " << u << "\n";
+        }
+    }
+    else {
+        std::cout << "\nNO VALID USER.\n";
+    }
+
+    return 0;
+}
